85980d0ae326d257a797f451556834c31e234a0f
Tan Tai <tantai9991@gmail.com>
2020-08-08 21:02:28 +0700

    Update routes.rb

--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #User Signup Controller :
+  resources :signup, only: :create
+  #User Login Controller :
+  resources :login, only: :index
+  # Photo, ALbums Controller :
+  resources :photo , :album
+  #feed,discovery Controller : homepage
+  get 'feed', to 'homepage#index'
+  get 'discovery', to 'homepage#discovery'
+
+  #Profile Controller : profile
+  get 'profile', to 'profile#index'
+
 end
