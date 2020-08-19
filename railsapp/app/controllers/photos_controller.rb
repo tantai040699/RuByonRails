618bd0f5569ea223c9ad4ad456b7fc263e48ddfb
Tantai <tantai9991@gmail.com>
2020-08-19 08:32:53 +0700

    kaminari

--- railsapp/app/controllers/photos_controller.rb
+++ railsapp/app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   def index
-    @photo = Photo.where("status = ?",'public').order(:created_at)
+    @photo = Photo.where("status = ?",'public').order(:created_at).page params[:page]
   end
   
 end
