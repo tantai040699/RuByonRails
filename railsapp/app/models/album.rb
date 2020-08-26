0c2f8fc95b6eb3f01a7e853ba17d5995b3274280
Tantai <tantai9991@gmail.com>
2020-08-26 15:24:58 +0700

    V1

--- railsapp/app/models/album.rb
+++ railsapp/app/models/album.rb
@@ -8,7 +8,7 @@ class Album < ApplicationRecord
     #upload file
     # validates :title, :desc, :status, :collection, presence: true
     # validates :title, presence: true ,length: {minimum:4,maximum:140}
-    # validates :status, acceptance: {accept: ['private','public']}
+    # validates :status, acceptance: {accept: ['0','1']}
     # validates :desc, length: {maximum:300}
     # validates :collection, numericality: {more_than: 1, less_than:25}
 
